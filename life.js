cb1853cad069e9bb8f5be07775e5daa848ed4d9a
Elizabeth <ojetunjielizabeth@yahoo.com>
2022-06-23 20:26:01 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,29 @@
+
+alert("This is a basic arithmetic calculator, press OK to continue")
+
+var number1 = parseFloat(prompt("Enter the first number: "))
+var operator = prompt("Operator: ")
+var number2 = parseFloat(prompt("Enter the second number: "))
+
+let a = number1 + number2
+let b = number1 - number2
+let c = number1 * number2
+let e = number1 / number2
+
+if (operator == "+")
+{
+    alert("The answer is: "+a)
+} 
+    else if (operator == "-")
+{
+    alert("The answer is: "+b)
+} 
+    else if (operator == "*")
+{
+    alert("The answer is: "+c)
+} 
+    else if (operator == "/")
+{
+    alert("The answer is: "+e)
+}
+
